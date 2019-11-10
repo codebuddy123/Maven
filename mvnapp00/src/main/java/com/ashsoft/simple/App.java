fa2f64c3affad9370322ec544d452877458e1e7e
Ashish <hangoutbuddy3@gmail.com>
2019-11-10 23:34:35 +0530

    Create,Compile and Execute Maven Project
    This is Manual approach without Using Eclipse

---
+++ mvnapp00/src/main/java/com/ashsoft/simple/App.java
@@ -0,0 +1,13 @@
+package com.ashsoft.simple;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! This is from Maven App" );
+    }
+}
---
+++ mvnapp00/src/test/java/com/ashsoft/simple/AppTest.java
@@ -0,0 +1,21 @@
+package com.ashsoft.simple;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+    	System.out.println("This is from Maven testApp");
+        assertTrue( true );
+    }
+}
